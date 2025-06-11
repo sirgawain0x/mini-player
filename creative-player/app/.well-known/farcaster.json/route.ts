c89f2409a0dd795a570805a87ef044be47ea6e19
sirgawain0x <bracy.gawain2014@gmail.com>
2025-06-11 12:49:39 -0400

    share
    - added share extension.

--- creative-player/app/.well-known/farcaster.json/route.ts
+++ creative-player/app/.well-known/farcaster.json/route.ts
@@ -27,6 +27,7 @@ export async function GET() {
       description: process.env.NEXT_PUBLIC_APP_DESCRIPTION,
       screenshotUrls: [],
       iconUrl: process.env.NEXT_PUBLIC_APP_ICON,
+      castShareUrl: `${URL}/share`,
       splashImageUrl: process.env.NEXT_PUBLIC_APP_SPLASH_IMAGE,
       splashBackgroundColor: process.env.NEXT_PUBLIC_SPLASH_BACKGROUND_COLOR,
       homeUrl: URL,
